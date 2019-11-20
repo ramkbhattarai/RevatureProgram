17d6980d37c8f83fea0a5130b4b3d03e634b873e
Ram <ramkb466@gmail.com>
2019-11-20 11:36:52 -0500

    first commit

---
+++ src/com/rkb/Hello.java
@@ -0,0 +1,21 @@
+package com.rkb;
+
+public class Hello {
+			// this is a comment
+	// anything written inside these, will be ignored
+	// Java will run applications beginning with a main method
+	// this is a method (behavior) named main
+	
+	// this is the syntax for the main method 
+	
+	public static void main(String[] args) {
+		
+		// this is the starting point for our application 
+		
+		
+		// In order to create output in the console  we use System.out.println
+		
+		System.out.println("Hello World! I have changed the program");
+	}
+	
+	}
